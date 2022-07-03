f2c80bc6629862f62859fb9e755e3014f17ecb67
s1vo <72389139+s1vo@users.noreply.github.com>
2022-07-03 21:03:05 +0300

    fixed errors

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,22 +4,23 @@ class MarvelService {
     _apiKey = 'apikey=cba4daddc3a557582bffa4682cfaadc7' 
 
     getResource = async (url) => {
-        let res = await fetch(url); 
-        if(!res.ok){
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
+    
         return await res.json();
     }
 
-    // получение всех персонажей 
     getAllCharacters = async () => {
         const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-        return res.data.results.map(this._transtormCharacter);
+        return res.data.results.map(this._transformCharacter);
     }
-    // получение персонажа по ID 
+
     getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transtormCharacter(res.data.results[0]); 
+        return this._transformCharacter(res.data.results[0]);
     }
 
     _transformCharacter = (char) => {
